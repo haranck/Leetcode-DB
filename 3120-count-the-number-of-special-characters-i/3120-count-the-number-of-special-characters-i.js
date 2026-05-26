70b1ed0594007a671a17a1bfdbb2e2594bfac2d2
Haran Krishna Ck <harankrishna03@gmail.com>
2026-05-26 11:10:17 +0530

    Time: 27 ms (9.52%), Space: 59.8 MB (6.35%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var numberOfSpecialChars = function (word) {
+    let count = 0
+    let set = new Set(word)
+    let visited = new Set()
+    console.log(set)
+
+    for (let char of word) {
+        let lower = char.toLowerCase()
+        if (
+            set.has(lower.toLowerCase()) &&
+            set.has(lower.toUpperCase()) &&
+            !visited.has(lower)
+        ) {
+            count++
+            visited.add(lower)
+        }
+    }
+    return count
+    console.log(count)
+};
