2379545c3c902798e4bf3638ae228e775fad3884
Haran Krishna Ck <harankrishna03@gmail.com>
2026-05-30 10:25:09 +0530

    Time: 1 ms (20.87%), Space: 52.8 MB (93.04%) - LeetHub

--- 0476-number-complement/0476-number-complement.js
+++ 0476-number-complement/0476-number-complement.js
@@ -1,7 +1,14 @@
-/**
- * @param {number} num
- * @return {number}
- */
 var findComplement = function(num) {
-    
+    let bit = num.toString(2);
+    let str = "";
+
+    for (let char of bit) {
+        if (char === "1") {
+            str += "0";
+        } else {
+            str += "1";
+        }
+    }
+
+    return parseInt(str, 2);
 };
