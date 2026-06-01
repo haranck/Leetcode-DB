1e328930e0a34c7aa194f8eaf3eb325813fe0651
Haran Krishna Ck <harankrishna03@gmail.com>
2026-06-01 10:03:43 +0530

    Time: 25 ms (7.14%), Space: 59.9 MB (7.14%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minimumCost = function (cost) {
+    cost.sort((a, b) => b - a)
+    console.log(cost)
+    let sum = cost.reduce((acc, curr) => acc + curr, 0)
+    for (let i = 2; i < cost.length; i += 3){
+        sum -= cost[i]
+    }
+    console.log(sum)
+    return sum
+};
