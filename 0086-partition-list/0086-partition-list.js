89abfb8b6654c7760cb293173eb048df24074919
Haran Krishna Ck <harankrishna03@gmail.com>
2026-06-08 20:33:27 +0530

    Time: 8 ms (2.5%), Space: 57.9 MB (19.17%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+    let smallDummy = new ListNode(0)
+    let largeDummy = new ListNode(0)
+
+    let small = smallDummy
+    let large = largeDummy
+    let current = head
+    while(current){
+        if(current.val < x) {
+            small.next = current
+            small = small.next
+        }else {
+            large.next = current
+            large = large.next
+        }
+        current = current.next
+    }
+    large.next = null
+    small.next = largeDummy.next
+    console.log(smallDummy.next)
+    return smallDummy.next
+};
