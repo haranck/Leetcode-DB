946df80aa9176df4f16309fd7614ea6bee6b6a89
Haran Krishna Ck <harankrishna03@gmail.com>
2026-07-01 20:46:08 +0530

    Time: 1 ms (58.38%), Space: 58.3 MB (72.21%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -10,6 +10,25 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root) {
-    
+var isValidBST = function (root) {
+    let node = root
+    function validBst(node, min, max) {
+        if (!node) return true
+
+        if (node.val >= max || node.val <= min) return false
+ 
+        return (
+            validBst(node.left, min, node.val) &&
+            validBst(node.right, node.val, max)
+        )
+    }
+    return validBst(node, min = -Infinity, max = Infinity)
 };
+
+
+
+
+
+
+
+
